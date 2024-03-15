3104fb5c697cf42a69ee9a4522b360f300228a98
Amad khan <162555776+AmadKhan95@users.noreply.github.com>
2024-03-14 23:52:08 -0700

    Add files via upload
    Helle :
    This is a Calculator project using: HTML, CSS, & JAVA Script.
    I hope this file will be helpful for you.
    Thanks.

---
+++ logic.js
@@ -0,0 +1,97 @@
+let fc;
+function my_fc(infield) {
+    fc = infield;
+} 
+function forone() {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function fortwo(value) {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;  
+}
+function forthree(value) {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function forfour(value) {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function forfive(value) {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function forsix(value) {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function forseven(value) {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function foreight(value) {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function fornine(value) {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function forzero(value) {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function fordot() {
+    let bt = document.activeElement.textContent;
+    let tb = document.getElementById(fc);
+    tb.value += bt;
+}
+function forac() {
+    document.getElementById("first").value = "" ;
+    document.getElementById("second").value = "" ;
+    document.getElementById("va").innerHTML = "" ;
+}
+function fordiv() {
+    let a = document.getElementById("first").value;
+    let b = document.getElementById("second").value;
+    document.getElementById("va").innerHTML = Number(a) / Number(b); 
+}
+function forplus() {
+    let a = document.getElementById("first").value;
+    let b = document.getElementById("second").value;
+    document.getElementById("va").innerHTML = Number(a) + Number(b); 
+}
+function forsub() {
+    let a = document.getElementById("first").value;
+    let b = document.getElementById("second").value;
+    document.getElementById("va").innerHTML = Number(a) - Number(b); 
+}
+function formul() {
+    let a = document.getElementById("first").value;
+    let b = document.getElementById("second").value;
+    document.getElementById("va").innerHTML = Number(a) * Number(b); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
